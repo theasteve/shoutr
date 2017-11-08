70c66c03f2be1b1d659757ee726a7ad544367a17
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-11-08 16:43:24 -0500

    redirect to dashboard when signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
-class UsersController < Clearance::UsersController
+class UsersController <  Clearance::UsersController
   private
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+   def new
+     @user = User.new
+     render template: "users/new"
+   end
+   
+   def user_params
+     params.require(:user).permit(:username, :email, :password)
+   end
 end
